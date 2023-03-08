33a9b6f7390447a6aabbdda1ed12c4676b4d731c
ozbakirsahincan <ozbakirsahincan@gmail.com>
2023-03-08 18:48:34 +0100

    category refactoring

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,44 @@ const router = require('express').Router();
 const db = require('../data/db');
 
 
-router.use("/blogs/:blogid", function (req, res) {
-    res.render("users/blog-details")
+router.use('/blogs/category/:categoryid', async function (req, res) {
+    const id = req.params.categoryid;
+    try {
+        const [blogs] = await db.execute('select * from blog where categoryid =?', [id]);
+        const [categories] = await db.execute("select * from category");
+        res.render("users/blogs", {
+            title: 'Tüm Kurslarım',
+            blogs: blogs,
+            categories: categories,
+            selectedCategory:id
+        })
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.use("/blogs/:blogid", async function (req, res) {
+    const id = req.params.blogid;
+    try {
+        const [blogs] = await db.execute('select * from blog where blogid =?', [id]);
+
+
+        const blog = blogs[0];
+        if (blog) {
+            return res.render("users/blog-details", {
+                title: blog.title,
+                blog: blog,
+                selectedCategory : null
+            })
+        }
+
+        res.redirect("/");
+
+    } catch (error) {
+        console.log(error);
+    }
+
+
 });
 router.use("/blogs", async function (req, res) {
     try {
@@ -13,7 +49,8 @@ router.use("/blogs", async function (req, res) {
         res.render("users/blogs", {
             title: 'Tüm Kurslarım',
             blogs: blogs,
-            categories: categories
+            categories: categories,
+            selectedCategory : null
         })
     } catch (err) {
         console.log(err);
@@ -28,6 +65,7 @@ router.use("/", async function (req, res) {
             title: "Popüler Kurslarım",
             blogs: blogs,
             categories: categories,
+            selectedCategory : null
         })
 
     } catch (err) {
